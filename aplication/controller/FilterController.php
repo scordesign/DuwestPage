9f97726cb3ddd8566b4a1d3a0d2442d5fefbbe58
andresolmos <80769478+andresolmos@users.noreply.github.com>
2024-03-02 18:33:22 -0500

    *

--- aplication/controller/FilterController.php
+++ aplication/controller/FilterController.php
@@ -9,18 +9,31 @@ class Filters
 
     public function getFilters() : String
     {
-        
         $returnFields = array();
-
-        $conexion = new Conexion();
+        try {
+            $conexion = new Conexion();
         $pdo = $conexion->obtenerConexion();
 
-        $statement = $pdo->prepare("SELECT * FROM filters");
+        $statement = $pdo->prepare("SELECT category FROM filters group by category");
         $statement->execute();
-        $resultado = $statement->fetchAll();
-        
 
-        $returnFields["data"] = $resultado;
+        $resultado = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        
+        
+        $categorysFilters = array();
+
+        foreach ($resultado as $category ) {
+            $statementIn = null;
+            $resultadoIn = null;
+            $statementIn = $pdo->prepare("SELECT * FROM filters where `category` = :category");
+            $statementIn->bindParam(':category', $category['category'] , PDO::PARAM_STR);
+            $statementIn->execute();
+            $resultadoIn = $statementIn->fetchAll(PDO::FETCH_ASSOC);
+            $categorysFilters[$category['category']] = $resultadoIn;
+        }
+
+        $returnFields["data"] = $categorysFilters;
         $returnFields["status"] = 200;
         $returnFields["message"] = "correcto";
 
@@ -28,10 +41,22 @@ class Filters
 
 
         return json_encode($returnProduct);
+        } catch (\Throwable $e) {
+            echo var_dump($e);
+            $returnFields["status"] = 500;
+        $returnFields["message"] = $e->getMessage();
+
+        $returnProduct = json_encode($returnFields);
+
+
+        return json_encode($returnProduct);
+        }
+        
+
+        
 
         // Iterar sobre el resultado
     }
-
 
 }
 ?>
--- aplication/controller/ProductsController.php
+++ aplication/controller/ProductsController.php
@@ -33,47 +33,51 @@ class Products
             $description = $_POST["description"] === null ? "" : $_POST["description"];
             $name = $_POST["name"] === null ? "" : $_POST["name"];
             $section = $_POST["section"] === null ? "" : $_POST["section"];
-
-            $directoryFile = "C:/Users/andres/Documents/prueba/" . $name . "/files";
-
-            $directoryimage = "C:/Users/andres/Documents/prueba/" . $name . "/images";
-
-            if (!is_dir($directoryFile)) {
-                // Crear la carpeta con permisos 0777 (lectura, escritura y ejecución para todos)
-                mkdir($directoryFile, 0777, true);
-
-                // Establecer permisos adicionales si es necesario
-                chmod($directoryFile, 0777);
+            $amount = $_POST["amount"] === null ? "" : $_POST["amount"];
+            $imagesNamesString ="[]";
+            if($images !== null){
+                $directoryimage = "C:/Users/andres/Documents/prueba/" . $name . "/images";
+                if (!is_dir($directoryimage)) {
+                    // Crear la carpeta con permisos 0777 (lectura, escritura y ejecución para todos)
+                    mkdir($directoryimage, 0777, true);
+    
+                    // Establecer permisos adicionales si es necesario
+                    chmod($directoryimage, 0777);
+                }
+                foreach ($images["name"] as $key => $nombre) {
+                    // Mover archivo al directorio deseado
+                    $rutaArchivo = $directoryimage . "/" . basename($images["name"][$key]);
+                    move_uploaded_file($images["tmp_name"][$key], $rutaArchivo);
+                    // Agregar nombre del archivo a la lista
+                    $imagesNames[] = $directoryimage . "/" . basename($images["name"][$key]);
+                }
+                $imagesNamesString = json_encode($imagesNames);
             }
+            $filesNamesString = "[]";
+            if($files !== null){
+                $directoryFile = "C:/Users/andres/Documents/prueba/" . $name . "/files";
 
-            if (!is_dir($directoryimage)) {
-                // Crear la carpeta con permisos 0777 (lectura, escritura y ejecución para todos)
-                mkdir($directoryimage, 0777, true);
+                if (!is_dir($directoryFile)) {
+                    // Crear la carpeta con permisos 0777 (lectura, escritura y ejecución para todos)
+                    mkdir($directoryFile, 0777, true);
+    
+                    // Establecer permisos adicionales si es necesario
+                    chmod($directoryFile, 0777);
+                }
 
-                // Establecer permisos adicionales si es necesario
-                chmod($directoryimage, 0777);
+                foreach ($files["name"] as $key => $nombre) {
+                    // Mover archivo al directorio deseado
+                    $rutaArchivo = $directoryFile . "/" . basename($files["name"][$key]);
+                    move_uploaded_file($files["tmp_name"][$key], $rutaArchivo);
+                    // Agregar nombre del archivo a la lista
+                    $filesNames[] = $directoryFile . "/" . basename($files["name"][$key]);
+                }
+
+                $filesNamesString = json_encode($filesNames);
             }
-
-
-            foreach ($files["name"] as $key => $nombre) {
-                // Mover archivo al directorio deseado
-                $rutaArchivo = $directoryFile . "/" . basename($files["name"][$key]);
-                move_uploaded_file($files["tmp_name"][$key], $rutaArchivo);
-                // Agregar nombre del archivo a la lista
-                $filesNames[] = $directoryFile . "/" . basename($files["name"][$key]);
-            }
-
-            foreach ($images["name"] as $key => $nombre) {
-                // Mover archivo al directorio deseado
-                $rutaArchivo = $directoryimage . "/" . basename($images["name"][$key]);
-                move_uploaded_file($images["tmp_name"][$key], $rutaArchivo);
-                // Agregar nombre del archivo a la lista
-                $imagesNames[] = $directoryimage . "/" . basename($images["name"][$key]);
-            }
-            $imagesNamesString = json_encode($imagesNames);
-            $filesNamesString = json_encode($filesNames);
+           
             
-            $stmt = $pdo->prepare("INSERT INTO products (`name`,`description`,`listImg`,`listDocs`,`filters`,`section`) VALUES (:name,:description,:listImg,:listDocs,:filters,:section)");
+            $stmt = $pdo->prepare("INSERT INTO products (`name`,`description`,`listImg`,`listDocs`,`filters`,`section`,`amount`) VALUES (:name,:description,:listImg,:listDocs,:filters,:section,:amount)");
 
 
             $stmt->bindParam(':name', $name);
@@ -82,6 +86,7 @@ class Products
             $stmt->bindParam(':listDocs', $filesNamesString);
             $stmt->bindParam(':filters', $filters);
             $stmt->bindParam(':section', $section);
+            $stmt->bindParam(':amount', $amount);
             // Ejecutar la sentencia SQL con los valores correspondientes
             $stmt->execute();
 
@@ -98,6 +103,113 @@ class Products
         } catch (\Throwable $e) {
             $returnFields["status"] = 500;
             $returnFields["message"] = $e->getMessage();
+
+            $returnProduct = json_encode($returnFields);
+
+
+            return json_encode($returnProduct);
+        }
+        // Iterar sobre el resultado
+    }
+
+    public function getProducts(): string
+    {
+        $returnFields = array();
+        try {
+            $page = !isset($_GET["page"]) ? 1 : $_GET["page"];
+            
+            $filters = !isset($_GET["filters"])  ? "" : substr($_GET["filters"], 0, strlen($_GET["filters"]) - 1);
+            $search =!isset($_GET["search"]) ? "" : $_GET["search"];
+
+            $Ssql = "select * from products where ";
+            if($search !== ""){
+                $search = '%' . $search . '%';
+                $Ssql .= "`name` = :name and ";
+            }
+
+            if($filters !== ""){
+                $filters = str_replace("{", "%", $filters);
+                    $filters = str_replace("}", "%", $filters);
+                $filtersList = explode(",", $filters);
+                $Ssql .= "(";
+                foreach($filtersList as $filter){
+                    $Ssql .= "`filters` = :filter"+str_replace("%", "", $filters)+" or ";
+                }
+                $Ssql = str_ends_with($Ssql , ' or ')? substr($Ssql, 0,strlen($Ssql) - 3):$Ssql;
+                $Ssql .= ")";
+            }
+            $Ssql = str_ends_with($Ssql , ' and ')? substr($Ssql, 0,strlen($Ssql) - 4):$Ssql;
+            $Ssql = str_ends_with($Ssql , ' where ')? substr($Ssql, 0,strlen($Ssql) - 6):$Ssql;
+
+            $conexion = new Conexion();
+            $pdo = $conexion->obtenerConexion();
+            
+            // count
+            $statementCount = $pdo->prepare(str_replace(" * ", " count(*) as count ", $Ssql));
+
+            if($search !== ""){
+                $statementCount->bindParam(':name', $search , PDO::PARAM_STR);
+            }
+
+            if($filters !== ""){
+                $filtersList = explode(",", $filters);
+                foreach($filtersList as $filter){
+                    $statementCount->bindParam( ":filter"+str_replace("%", "", $filters) , $filter, PDO::PARAM_STR);
+                }
+            }
+
+
+
+            $statementCount->execute();
+            $resultadoCount = $statementCount->fetchAll(PDO::FETCH_ASSOC);
+
+            $count = $resultadoCount[0]["count"];
+            
+
+            // data
+            $statement = $pdo->prepare($Ssql." LIMIT ". $page * 10 ." OFFSET 0" . ($page * 10) -10);
+
+            if($search !== ""){
+                $statement->bindParam(':name', $search , PDO::PARAM_STR);
+            }
+
+            if($filters !== ""){
+                $filtersList = explode(",", $filters);
+                foreach($filtersList as $filter){
+                    $statement->bindParam( ":filter"+str_replace("%", "", $filters) , $filter, PDO::PARAM_STR);
+                }
+            }
+
+            $statement->execute();
+            $resultados = $statement->fetchAll(PDO::FETCH_ASSOC);
+            $resultadosReturn = array();
+
+             $i = 0;
+            foreach($resultados as $resultado){
+                $resultadosReturnEach = array();
+                $resultadosReturnEach['listDocs'] = json_decode($resultado['listDocs'], true);
+                $resultadosReturnEach['listImg'] = json_decode($resultado['listImg'], true);
+                $resultadosReturnEach['name'] = $resultado['name'];
+                $resultadosReturnEach['description'] = $resultado['description'];
+                $resultadosReturnEach['amount'] = $resultado['amount'];
+                $resultadosReturn[$i] =  $resultadosReturnEach;
+                $i++;
+            }
+
+            $returnFields["data"] = $resultadosReturn ;
+            $returnFields["Page"] = $page;
+            $returnFields["Total"] = $count;
+            $returnFields["status"] = 200;
+            $returnFields["message"] = "Correcto";
+
+            $returnProduct = json_encode($returnFields);
+
+
+            return json_encode($returnProduct);
+
+        } catch (\Throwable $e) {
+            $returnFields["status"] = 500;
+            $returnFields["message"] = $e;
 
             $returnProduct = json_encode($returnFields);
 
