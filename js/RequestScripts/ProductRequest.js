89156085db9f7464687bf8270f746275a91eea41
andresolmos <80769478+andresolmos@users.noreply.github.com>
2024-04-09 21:42:43 -0500

    products

--- js/RequestScripts/ProductRequest.js
+++ js/RequestScripts/ProductRequest.js
@@ -1,153 +1,4 @@
 $(function () {
-    $("#addProduct").on("click", function () {
-
-        $("#modalBackground").toggleClass("hide");
-
-        getSession().then(function (session) {
-            $('#formModal').html("");
-            if (session == "[]" || session == "") {
-                session = JSON.parse(session);
-
-                var form = $("<form>").attr("id", "miFormulario");
-
-
-                form.append($("<i>").attr("font-size", "20vh").attr("class", "fas fa-times-circle").attr("style", "color: #32aa48; font-size:23vh;"));
-
-                form.append($("<label>").text("error: "));
-                form.append($("<input>").attr("type", "text").attr("disabled", "disabled").attr("value", "Sesión no iniciada").attr("style", "text-align: center;"));
-
-                // Agregar el formulario al cuerpo del documento
-                $("#formModal").append(form);
-
-                return;
-            }
-
-
-
-            var form = $("<form>").attr("id", "miFormulario");
-
-
-            form.append($("<label>").text("Nombre del producto: "));
-            form.append($("<input>").attr("type", "text").attr("name", "name").attr("required", "required"));
-
-            form.append($("<label>").text("Cantidad del producto: "));
-            form.append($("<input>").attr("type", "text").attr("name", "amount"));
-
-            form.append($("<label>").text("Descripción del producto: "));
-            form.append($("<textarea>").attr("name", "description").attr("style", "width: calc(100% - 20px);"));
-
-            form.append($("<label>").text("Imagenes del producto: "));
-            form.append($("<input>").attr("type", "file").attr("name", "images[]").attr("multiple", "multiple").attr("style", "color:black;"));
-
-            form.append($("<label>").text("Archivos asociados al producto: "));
-            form.append($("<input>").attr("type", "file").attr("name", "files[]").attr("multiple", "multiple").attr("style", "color:black;"));
-
-            var div = $("<div>").attr("id", "filters").attr("style", "margin-top:2%;");
-
-            $.ajax({
-                url: "aplication/RequestController.php?action=getfilters", // Archivo PHP que contiene la función
-                type: "GET", // Método de solicitud
-                success: function (response) {
-                    response = JSON.parse(JSON.parse(response));
-
-                    for (var key in response.data) {
-                        var divFirst = $("<div>").attr("class", "filters noClose").attr("style", "width:100%;color:black;");
-
-                        if (response.data.hasOwnProperty(key)) {
-                            divFirst.append($("<h4>").html(key).attr("class", "bold noClose"));
-                            var objetos = response.data[key];
-                            // Iterar sobre los objetos dentro de cada categoría
-                            objetos.forEach(function (element) {
-                                var divData = $("<div>").attr("class", "filtersEach").attr("style", "display:inline-flex;width:50%;");
-
-                                divData.append($("<input>").attr("type", "checkbox").attr("onclick", "filterAdd(" + element.id + ")").attr("name", element.name).attr("id", "checkBox-" + element.id).attr("style", "width:20%;"));
-                                divData.append($("<label>").text(element.name).attr("styles", "width:80%;"));
-
-                                divFirst.append(divData);
-                            });
-                            div.append(divFirst);
-                        }
-                    }
-
-
-                },
-                error: function (xhr, status, error) {
-                    // Manejar errores
-                    console.log(xhr.responseText); // Mostrar la respuesta del servidor en la consola
-                }
-            });
-
-            form.append(div);
-
-
-
-            form.append($("<input>").attr("type", "text").attr("name", "filters").attr("id", "filtersInput").attr("hidden", "hidden"));
-
-            form.append($("<input>").attr("type", "text").attr("name", "section").attr("hidden", "hidden").attr("value", currentPageID));
-
-            form.append($("<input>").attr("type", "text").attr("name", "action").attr("hidden", "hidden").attr("value", "addProduct"));
-
-
-            form.append($("<button>").attr("type", "submit").text("Registar nuevo Producto").attr("id", "addProduct"));
-
-
-            // Agregar el formulario al cuerpo del documento
-            $("#formModal").append(form);
-
-            // Manejar el envío del formulario
-            $("#miFormulario").submit(function (event) {
-                event.preventDefault(); // Prevenir el envío del formulario normal
-                formData = new FormData(this);
-                // Hacer una llamada AJAX al archivo PHP
-                $.ajax({
-                    url: "aplication/RequestController.php", // Archivo PHP que contiene la función
-                    type: "POST", // Método de solicitud
-                    data: formData,
-                    contentType: false,
-                    processData: false,// Datos a enviar (datos del formulario serializados)
-                    success: function (response) {
-                        // Manejar la respuesta
-                        response = JSON.parse(JSON.parse(response));
-                        $("#alerta").removeClass("bg-success");
-                        $("#alerta").removeClass("bg-danger");
-                        $("#alerta").removeClass("bg-warning");
-                        $("#alerta").html(response.message);
-                        if (response.status == 200) {
-                            $("#alerta").addClass("bg-success");
-                        } else if (response.status == 500) {
-                            $("#alerta").addClass("bg-danger");
-                        } else {
-                            $("#alerta").addClass("bg-warning");
-                        }
-
-                        $("#modalBackground").toggleClass("hide");
-                        chargeProducts(currentPageID.replace("#tm-section-", "").trim())
-
-                        // Mostrar la alerta
-                        $("#alerta").fadeIn();
-
-
-                        // Desvanecer la alerta después de 3 segundos
-                        setTimeout(function () {
-                            $("#alerta").fadeOut();
-                        }, 3000);
-
-                    },
-                    error: function (xhr, status, error) {
-                        // Manejar errores
-                        console.log(xhr.responseText); // Mostrar la respuesta del servidor en la consola
-                    }
-                });
-            });
-        }).catch(function (error) {
-            // Ha ocurrido un error al obtener la sesión
-            console.log("Error al obtener la sesión:", error);
-        });
-
-    });
-
-
-
     $("#searchMenu").keyup(function () {
         pageId = currentPageID.replace("#tm-section-", "").trim();
         $("#products2-" + pageId + " .divProduct").remove();
@@ -156,6 +7,154 @@ $(function () {
 
 });
 
+function addProduct() {
+    $("#modalBackground").toggleClass("hide");
+
+    getSession().then(function (session) {
+        $('#formModal').html("");
+        if (session == "[]" || session == "") {
+            session = JSON.parse(session);
+
+            var form = $("<form>").attr("id", "miFormulario");
+
+
+            form.append($("<i>").attr("font-size", "20vh").attr("class", "fas fa-times-circle").attr("style", "color: #32aa48; font-size:23vh;"));
+
+            form.append($("<label>").text("error: "));
+            form.append($("<input>").attr("type", "text").attr("disabled", "disabled").attr("value", "Sesión no iniciada").attr("style", "text-align: center;"));
+
+            // Agregar el formulario al cuerpo del documento
+            $("#formModal").append(form);
+
+            return;
+        }
+
+
+
+        var form = $("<form>").attr("id", "miFormulario");
+
+
+        form.append($("<label>").text("Nombre del producto: "));
+        form.append($("<input>").attr("type", "text").attr("name", "name").attr("required", "required"));
+
+        form.append($("<label>").text("Cantidad del producto: "));
+        form.append($("<input>").attr("type", "text").attr("name", "amount"));
+
+        form.append($("<label>").text("Descripción del producto: "));
+        form.append($("<textarea>").attr("name", "description").attr("style", "width: calc(100% - 20px);"));
+
+        form.append($("<label>").text("Imagenes del producto: "));
+        form.append($("<input>").attr("type", "file").attr("name", "images[]").attr("multiple", "multiple").attr("style", "color:black;"));
+
+        form.append($("<label>").text("Archivos asociados al producto: "));
+        form.append($("<input>").attr("type", "file").attr("name", "files[]").attr("multiple", "multiple").attr("style", "color:black;"));
+
+        var div = $("<div>").attr("id", "filters").attr("style", "margin-top:2%;");
+
+        $.ajax({
+            url: "aplication/RequestController.php?action=getfilters", // Archivo PHP que contiene la función
+            type: "GET", // Método de solicitud
+            success: function (response) {
+                response = JSON.parse(JSON.parse(response));
+
+                for (var key in response.data) {
+                    var divFirst = $("<div>").attr("class", "filters noClose").attr("style", "width:100%;color:black;");
+
+                    if (response.data.hasOwnProperty(key)) {
+                        divFirst.append($("<h4>").html(key).attr("class", "bold noClose"));
+                        var objetos = response.data[key];
+                        // Iterar sobre los objetos dentro de cada categoría
+                        objetos.forEach(function (element) {
+                            var divData = $("<div>").attr("class", "filtersEach").attr("style", "display:inline-flex;width:50%;");
+
+                            divData.append($("<input>").attr("type", "checkbox").attr("onclick", "filterAdd(" + element.id + ")").attr("name", element.name).attr("id", "checkBox-" + element.id).attr("style", "width:20%;"));
+                            divData.append($("<label>").text(element.name).attr("styles", "width:80%;"));
+
+                            divFirst.append(divData);
+                        });
+                        div.append(divFirst);
+                    }
+                }
+
+
+            },
+            error: function (xhr, status, error) {
+                // Manejar errores
+                console.log(xhr.responseText); // Mostrar la respuesta del servidor en la consola
+            }
+        });
+
+        if (!(currentPageID.trim().endsWith("14") ||currentPageID.trim().endsWith("15"))) {
+            form.append(div);
+        }
+
+
+
+        form.append($("<input>").attr("type", "text").attr("name", "filters").attr("id", "filtersInput").attr("hidden", "hidden"));
+
+        form.append($("<input>").attr("type", "text").attr("name", "section").attr("hidden", "hidden").attr("value", currentPageID));
+
+        form.append($("<input>").attr("type", "text").attr("name", "action").attr("hidden", "hidden").attr("value", "addProduct"));
+
+
+        form.append($("<button>").attr("type", "submit").text("Registar nuevo Producto").attr("id", "addProduct"));
+
+
+        // Agregar el formulario al cuerpo del documento
+        $("#formModal").append(form);
+
+        // Manejar el envío del formulario
+        $("#miFormulario").submit(function (event) {
+            event.preventDefault(); // Prevenir el envío del formulario normal
+            formData = new FormData(this);
+            // Hacer una llamada AJAX al archivo PHP
+            $.ajax({
+                url: "aplication/RequestController.php", // Archivo PHP que contiene la función
+                type: "POST", // Método de solicitud
+                data: formData,
+                contentType: false,
+                processData: false,// Datos a enviar (datos del formulario serializados)
+                success: function (response) {
+                    // Manejar la respuesta
+                    response = JSON.parse(JSON.parse(response));
+                    $("#alerta").removeClass("bg-success");
+                    $("#alerta").removeClass("bg-danger");
+                    $("#alerta").removeClass("bg-warning");
+                    $("#alerta").html(response.message);
+                    if (response.status == 200) {
+                        $("#alerta").addClass("bg-success");
+                    } else if (response.status == 500) {
+                        $("#alerta").addClass("bg-danger");
+                    } else {
+                        $("#alerta").addClass("bg-warning");
+                    }
+
+                    $("#modalBackground").toggleClass("hide");
+                    chargeProducts(currentPageID.replace("#tm-section-", "").trim())
+
+                    // Mostrar la alerta
+                    $("#alerta").fadeIn();
+
+
+                    // Desvanecer la alerta después de 3 segundos
+                    setTimeout(function () {
+                        $("#alerta").fadeOut();
+                    }, 3000);
+
+                },
+                error: function (xhr, status, error) {
+                    // Manejar errores
+                    console.log(xhr.responseText); // Mostrar la respuesta del servidor en la consola
+                }
+            });
+        });
+    }).catch(function (error) {
+        // Ha ocurrido un error al obtener la sesión
+        console.log("Error al obtener la sesión:", error);
+    });
+}
+
+
 function pagination() {
     var pageCurrent = localStorage.getItem("PageRegs");
     var total = localStorage.getItem("TotalRegs");
@@ -163,13 +162,32 @@ function pagination() {
     var el = $("#products2-" + pageId);
     console.log(pageCurrent * 10);
     console.log(total);
-    console.log(el.offset().top  );
-    console.log( el.outerHeight(true));
+    console.log(el.offset().top);
+    console.log( el.height());
     console.log( $(window).height());
-    if ((el.offset().top + el.outerHeight(true) < $(window).height()) && (pageCurrent * 10) < total) {
+    if ((el.offset().top + el.height() < $(window).height()) && (pageCurrent * 10) < total) {
         localStorage.setItem("PageRegs", (pageCurrent.valueOf() - 1) + 2);
+        console.log("lo hizo");
+        getProducts(pageId, ($("#searchMenu").val() == "" ? undefined : $("#searchMenu").val()), ($("#filtersInput-" + pageId).val() == "" ? undefined : $("#filtersInput-" + pageId).val()), (pageCurrent.valueOf() - 1) + 2)
+        return;
+    }
+    console.log("no lo hizo");
+}
 
+function pagination14() {
+    var pageCurrent = localStorage.getItem("PageRegs");
+    var total = localStorage.getItem("TotalRegs");
+    var pageId = currentPageID.replace("#tm-section-", "").trim();
+    var fatherEl = $("#products2-" + pageId);
+    var el = $("#products2-"+pageId+" .functionPrducts")
 
+    console.log(pageCurrent * 10);
+    console.log(total);
+    console.log(el.offset().top);
+    console.log( el.height());
+    console.log( $(window).height());
+    if ((el.offset().top + el.height() < $(fatherEl).height()) && (pageCurrent * 10) < total) {
+        localStorage.setItem("PageRegs", (pageCurrent.valueOf() - 1) + 2);
         console.log("lo hizo");
         getProducts(pageId, ($("#searchMenu").val() == "" ? undefined : $("#searchMenu").val()), ($("#filtersInput-" + pageId).val() == "" ? undefined : $("#filtersInput-" + pageId).val()), (pageCurrent.valueOf() - 1) + 2)
         return;
